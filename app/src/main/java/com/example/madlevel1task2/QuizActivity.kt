a7c48bf7fcf3b08617a6fd15923b6114e0947cfa
Acdaling <acdaling@gmail.com>
2020-09-03 20:40:51 +0200

    Corrected Conjunction enum values and  correct/incorrect toast

--- app/src/main/java/com/example/madlevel1task2/QuizActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/QuizActivity.kt
@@ -1,20 +1,21 @@
 package com.example.madlevel1task2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.madlevel1task2.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var binding: ActivityQuizBinding
 
-    var lblQuestionOne: String = ConjunctionLetters.A.toString();
-    var lblQuestionTwo: String = ConjunctionLetters.B.toString();
-    var lblQuestionThree: String = ConjunctionLetters.B.toString();
-    var lblQuestionFour: String = ConjunctionLetters.B.toString();
-    var lblQuestionFive: String = ConjunctionLetters.B.toString();
-    var lblQuestionSix: String = ConjunctionLetters.B.toString();
-    var lblQuestionSeven: String = ConjunctionLetters.B.toString();
-    var lblQuestionEight: String = ConjunctionLetters.B.toString();
+    var lblQuestionOne: String = ConjunctionLetters.T.toString();
+    var lblQuestionTwo: String = ConjunctionLetters.F.toString();
+    var lblQuestionThree: String = ConjunctionLetters.F.toString();
+    var lblQuestionFour: String = ConjunctionLetters.F.toString();
+    var lblQuestionFive: String = ConjunctionLetters.F.toString();
+    var lblQuestionSix: String = ConjunctionLetters.F.toString();
+    var lblQuestionSeven: String = ConjunctionLetters.F.toString();
+    var lblQuestionEight: String = ConjunctionLetters.F.toString();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +25,7 @@ class QuizActivity : AppCompatActivity() {
     }
 
     private enum class ConjunctionLetters {
-        A, B
+        T, F
     }
 
     private fun chooseRandomLetter(): String {
@@ -48,15 +49,21 @@ class QuizActivity : AppCompatActivity() {
 
     private fun updateView() {
         updateQuestions()
-        binding.lblQuestionOne.text = getString(R.string.ConjuctionView_question_label_one, lblQuestionOne)
-        binding.lblQuestionTwo.text = getString(R.string.ConjuctionView_question_label_two, lblQuestionTwo)
-        binding.lblQuestionThree.text = getString(R.string.ConjuctionView_question_label_three, lblQuestionThree)
-        binding.lblQuestionFour.text = getString(R.string.ConjuctionView_question_label_four, lblQuestionFour)
-        binding.lblQuestionFive.text = getString(R.string.ConjuctionView_question_label_five, lblQuestionFive)
-        binding.lblQuestionSix.text = getString(R.string.ConjuctionView_question_label_six, lblQuestionSix)
-        binding.lblQuestionSeven.text = getString(R.string.ConjuctionView_question_label_seven, lblQuestionSeven)
-        binding.lblQuestionEight.text = getString(R.string.ConjuctionView_question_label_eight, lblQuestionEight)
+        binding.lblQuestionOne.text = getString(R.string.ConjunctionView_question_label_one, lblQuestionOne)
+        binding.lblQuestionTwo.text = getString(R.string.ConjunctionView_question_label_two, lblQuestionTwo)
+        binding.lblQuestionThree.text = getString(R.string.ConjunctionView_question_label_three, lblQuestionThree)
+        binding.lblQuestionFour.text = getString(R.string.ConjunctionView_question_label_four, lblQuestionFour)
+        binding.lblQuestionFive.text = getString(R.string.ConjunctionView_question_label_five, lblQuestionFive)
+        binding.lblQuestionSix.text = getString(R.string.ConjunctionView_question_label_six, lblQuestionSix)
+        binding.lblQuestionSeven.text = getString(R.string.ConjunctionView_question_label_seven, lblQuestionSeven)
+        binding.lblQuestionEight.text = getString(R.string.ConjunctionView_question_label_eight, lblQuestionEight)
     }
 
+    private fun onAnswerCorrect(){
+        Toast.makeText(this,getString(R.string.ConjunctionView_answer_correct), Toast.LENGTH_SHORT).show()
+    }
 
+    private fun onAnswerInCorrect(){
+        Toast.makeText(this,getString(R.string.ConjunctionView_answer_incorrect), Toast.LENGTH_SHORT).show()
+    }
 }
